11afd6cc473dff953a86dfd1193169d7aec3befe
FDongFDong <abnormal5626@naver.com>
2022-11-18 18:07:26 +0900

    null & undefined

---
+++ basic-types/null.ts
@@ -0,0 +1,8 @@
+// let MyName: string = null;
+// strictNullChecks가 true로 되어 있어 에러 처리
+
+// let u: undefined = null;
+let v: void = undefined;
+
+let union: string | null = null; // 합집합처럼 사용할 수 있다. -> union
+union = 'FDong';
